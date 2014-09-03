dacd1f119314be5b76e8d5d19166b5efb1be1df8
deadprogram <ron@hybridgroup.com>
2014-09-02 20:01:48 -0700

    Can blink on pin 7

--- examples/galileo_blink.js
+++ examples/galileo_blink.js
@@ -2,7 +2,7 @@ var Cylon = require('cylon');
 
 Cylon.robot({
   connection: { name: 'galileo', adaptor: 'galileo' },
-  device: {name: 'led', driver: 'led', pin: '13'},
+  device: {name: 'led', driver: 'led', pin: '7'},
 
   work: function(my) {
     every((1).second(), function() {
